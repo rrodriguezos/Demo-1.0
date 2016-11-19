3e846aee81e3f9f56def4c0a69225611f5e81f57
macrufpal <macrufpal@7486de1a-756a-4eb4-a4b8-0d1b24fd22ac>
2016-11-19 03:09:39 +0000

    Test

--- Acme-Demos-1.0/src/test/java/services/CommentServiceTest.java
+++ Acme-Demos-1.0/src/test/java/services/CommentServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import domain.Comment;
 
@@ -42,5 +43,69 @@ public class CommentServiceTest extends AbstractTest {
 		for(Comment com : commentService.findAll())
 			System.out.println(com.getAuthor() + ": " + com.getText() + " (" +com.getMoment() +")");
 	}
-
+	
+	
+	/*
+	 * An actor who is authenticated as an administrator must be able to
+	 * delete a comment that he or she finds inappropriate
+	 */
+	@Test
+	public void testPositiveDelete(){
+		Integer numCommentsOld;
+		Integer numCommentsNew;
+		
+		authenticate("admin");
+		
+		numCommentsOld = commentService.findAll().size();
+		commentService.delete(30);
+		numCommentsNew = commentService.findAll().size();
+		
+		Assert.isTrue(numCommentsNew == numCommentsOld - 1);
+		
+		unauthenticate();
+	}
+	
+	/*
+	 * An actor who is authenticated as an administrator must be able to
+	 * delete a comment that he or she finds inappropriate
+	 * 
+	 * Error: Admin not logged
+	 */
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void testNegativeDeleteWithoutAdmin(){
+		Integer numCommentsOld;
+		Integer numCommentsNew;
+		
+		unauthenticate();
+		
+		numCommentsOld = commentService.findAll().size();
+		commentService.delete(30);
+		numCommentsNew = commentService.findAll().size();
+		
+		Assert.isTrue(numCommentsNew == numCommentsOld - 1);
+		
+		unauthenticate();
+	}
+	
+	/*
+	 * An actor who is authenticated as an administrator must be able to
+	 * delete a comment that he or she finds inappropriate
+	 * 
+	 * Error: Comment not exist
+	 */
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void testNegativeDeleteCommentNotExist(){
+		Integer numCommentsOld;
+		Integer numCommentsNew;
+		
+		unauthenticate();
+		
+		numCommentsOld = commentService.findAll().size();
+		commentService.delete(1000);
+		numCommentsNew = commentService.findAll().size();
+		
+		Assert.isTrue(numCommentsNew == numCommentsOld - 1);
+		
+		unauthenticate();
+	}
 }
--- Acme-Demos-1.0/src/test/java/services/InvestmentServiceTest.java
+++ Acme-Demos-1.0/src/test/java/services/InvestmentServiceTest.java
@@ -9,10 +9,10 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-
-import domain.Investment;
+import org.springframework.util.Assert;
 
 import utilities.AbstractTest;
+import domain.Investment;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:spring/datasource.xml", "classpath:spring/config/packages.xml" })
@@ -39,6 +39,83 @@ public class InvestmentServiceTest extends AbstractTest {
 		for(Investment i : inv){
 			System.out.println(i.getDescription());
 		}
+	}
+	
+	
+	/*
+	 * An actor who is authenticated as an investor must be able to
+	 * create an investment regarding an existing demo
+	 */
+	@Test
+	public void testPositiveCreateInvestment(){
+		Investment investment;
+		Integer numInvestmentOld;
+		Integer numInvestmentNew;
+		
+		authenticate("investor1");
+		
+		investment = investmentService.create(16);
+		investment.setDescription("Descripcion de prueba");
+		
+		
+		numInvestmentOld = investmentService.findAll().size();
+		investmentService.save(investment);
+		numInvestmentNew = investmentService.findAll().size();
+		
+		Assert.isTrue(numInvestmentNew == numInvestmentOld + 1);
+		
+		unauthenticate();
+	}
+	
+	/*
+	 * An actor who is authenticated as an investor must be able to
+	 * create an investment regarding an existing demo
+	 * 
+	 * Error: Fields blank
+	 */
+	@Test(expected = javax.validation.ConstraintViolationException.class)
+	public void testNegativeCreateInvestmentFieldsBlank(){
+		Investment investment;
+		Integer numInvestmentOld;
+		Integer numInvestmentNew;
+		
+		authenticate("investor1");
+		
+		investment = investmentService.create(16);
+		
+		numInvestmentOld = investmentService.findAll().size();
+		investmentService.save(investment);
+		numInvestmentNew = investmentService.findAll().size();
+		
+		Assert.isTrue(numInvestmentNew == numInvestmentOld + 1);
+		
+		unauthenticate();
+	}
+	
+	/*
+	 * An actor who is authenticated as an investor must be able to
+	 * create an investment regarding an existing demo
+	 * 
+	 * Error: Investor not logged
+	 */
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void testNegativeCreateInvestmentWithoutInvestor(){
+		Investment investment;
+		Integer numInvestmentOld;
+		Integer numInvestmentNew;
+		
+		unauthenticate();
+		
+		investment = investmentService.create(16);
+		investment.setDescription("Prueba");
+		
+		numInvestmentOld = investmentService.findAll().size();
+		investmentService.save(investment);
+		numInvestmentNew = investmentService.findAll().size();
+		
+		Assert.isTrue(numInvestmentNew == numInvestmentOld + 1);
+		
+		unauthenticate();
 	}
 
 }
